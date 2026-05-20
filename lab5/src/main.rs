a935f68b97514f301549cc7d70aaa2c9abf496c5
Dx-by-Dy <alsmirnov2004@gmail.com>
2026-05-20 22:28:17 +0300

    HW5_A3

--- lab5/src/main.rs
+++ lab5/src/main.rs
@@ -1,7 +1,9 @@
+use base64::{Engine, engine::general_purpose};
 use std::{
-    env,
+    env, fs,
     io::{BufRead, BufReader, Write},
     net::TcpStream,
+    path::Path,
 };
 
 fn read_response(reader: &mut BufReader<TcpStream>) -> std::io::Result<String> {
@@ -27,8 +29,9 @@ fn send_command(
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
+
     if args.len() < 6 {
-        eprintln!("Usage: {} <server> <port> <from> <to> <message>", args[0]);
+        eprintln!("Usage: {} <server> <port> <from> <to> <image>", args[0]);
         std::process::exit(1);
     }
 
@@ -36,7 +39,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let port = &args[2];
     let from = &args[3];
     let to = &args[4];
-    let message = &args[5];
+    let image_path = &args[5];
 
     let address = format!("{server}:{port}");
     let mut stream = TcpStream::connect(address)?;
@@ -53,22 +56,42 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     send_command(&mut stream, &mut reader, &format!("RCPT TO:<{}>\r\n", to))?;
     send_command(&mut stream, &mut reader, "DATA\r\n")?;
 
-    let data = format!(
-        "Subject: Test message\r\n\
-         From: {}\r\n\
-         To: {}\r\n\
-         Content-Type: text/plain; charset=UTF-8\r\n\
-         \r\n\
-         {}\r\n\
-         .\r\n",
-        from, to, message
+    let image_data = fs::read(image_path)?;
+    let encoded = general_purpose::STANDARD.encode(image_data);
+    let filename = Path::new(image_path).file_name().unwrap().to_string_lossy();
+    let boundary = "BOUNDARY";
+
+    let message = format!(
+        concat!(
+            "Subject: Image attachment\r\n",
+            "From: {}\r\n",
+            "To: {}\r\n",
+            "MIME-Version: 1.0\r\n",
+            "Content-Type: multipart/mixed; boundary={}\r\n",
+            "\r\n",
+            "--{}\r\n",
+            "Content-Type: text/plain; charset=UTF-8\r\n",
+            "\r\n",
+            "Text part of the message.\r\n",
+            "\r\n",
+            "--{}\r\n",
+            "Content-Type: image/jpeg\r\n",
+            "Content-Transfer-Encoding: base64\r\n",
+            "Content-Disposition: attachment; filename=\"{}\"\r\n",
+            "\r\n",
+            "{}\r\n",
+            "\r\n",
+            "--{}--\r\n",
+            ".\r\n"
+        ),
+        from, to, boundary, boundary, boundary, filename, encoded, boundary
     );
-    print!("CLIENT: {}", data);
 
-    stream.write_all(data.as_bytes())?;
+    print!("CLIENT:\n{}", message);
+    stream.write_all(message.as_bytes())?;
     stream.flush()?;
-    read_response(&mut reader)?;
 
+    read_response(&mut reader)?;
     send_command(&mut stream, &mut reader, "QUIT\r\n")?;
 
     Ok(())
